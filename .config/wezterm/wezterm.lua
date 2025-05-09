8ef445f2e4b2d4eb8021b91086d14de384479358
Terminator15474 <jandommert8@gmail.com>
2025-05-09 16:24:37 +0200

    fixed C-Space for blink.cmp

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,19 @@
 local wezterm = require('wezterm') --[[@as Wezterm]]
 local config = wezterm.config_builder()
 
+config.automatically_reload_config = true
+
 config.default_prog = { 'zsh' }
+config.window_background_opacity = 0.2
+
+
+
+config.keys = {
+	{
+		key = 'Space',
+		mods = 'CTRL',
+		action = wezterm.action.SendKey { key = 'Space', mods = 'CTRL' },
+	}
+}
 
 return config
